205e7ceb0467e45d79bd2d516fc6f6e5cf9b8564
JMP160880 <javiermoranpando@gmail.com>
2023-02-11 16:03:49 +0000

    comenzando

--- src/models.py
+++ src/models.py
@@ -14,6 +14,7 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    
 
 class Address(Base):
     __tablename__ = 'address'
